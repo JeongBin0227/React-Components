5712aa8e8fa9581f7fcc42c17b12ecd37ace5c0d
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-07-31 21:24:57 +0900

    feat: modal component

--- modal/src/App.js
+++ modal/src/App.js
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { MyComponent } from "./MyComponent.js";
+import { MyComponent, MyModal } from "./MyComponent.js";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className="App">
-      <MyComponent />
+      {/* <MyComponent /> */}
+      <button onClick={openModal}>모달팝업</button>
+      <MyModal open={modalOpen} close={closeModal} header="Modal heading">
+        팝업창입니다. 쉽게 만들 수 있어요. 같이 만들어봐요!
+      </MyModal>
     </div>
   );
 }
